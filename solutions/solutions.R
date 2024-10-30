cb08377f3de91f554a32cb6070c1cce43ba9b647
jeremiaheb <jblondeau@gmail.com>
2024-10-30 13:56:28 -0400

    change week number and minor edits

--- solutions/solutions.R
+++ solutions/solutions.R
@@ -1,82 +1,26 @@
-
-
-# USVI <- getRvcData(2017:2021, c("STTSTJ", "STX"))
-library(gridExtra)
-library(grid)
 library(rvc)
 library(tidyverse)
+library(broom)
 
-plot_density <- function(df, spp) {
-  
-  a <- getDomainDensity(df, spp) %>% 
-        mutate( SE = sqrt(var),
-                YEAR = as.factor(YEAR),
-                REGION = as.factor(REGION)) %>% 
-                left_join(select(df$taxonomic_data, c("SPECIES_CD", "COMNAME")))
-  
-  p   <-  ggplot(a, aes(YEAR, density, group = REGION, color = REGION)) +
-            geom_point() +
-            geom_line() +
-            geom_errorbar(aes(ymin = density - SE, ymax = density + SE), width = 0.35) +
-            ylab("Density") +
-            xlab("Year")
-  
-  return(p)
-  
-}
 
-plot_occurrence <- function(df, spp) {
-  
-  a <- getDomainOccurrence(df, spp) %>% 
-    mutate( SE = sqrt(var),
-            YEAR = as.factor(YEAR),
-            REGION = as.factor(REGION)) %>% 
-    left_join(select(df$taxonomic_data, c("SPECIES_CD", "COMNAME")))
-  
-  p   <-  ggplot(a, aes(YEAR, occurrence, group = REGION, color = REGION)) +
-    geom_point() +
-    geom_line() +
-    geom_errorbar(aes(ymin = occurrence - SE, ymax = occurrence + SE), width = 0.35) +
-    ylab("Occurrence") +
-    xlab("Year")
-  
-  return(p)
-  
-}
+drto_psu_density <- read_csv("data/drto_psu_denisty.csv")
 
-plot_biomass <- function(df, spp) {
-  
-  a <- getDomainBiomass(df, spp) %>% 
-    mutate( SE = sqrt(var),
-            YEAR = as.factor(YEAR),
-            REGION = as.factor(REGION)) %>% 
-    left_join(select(df$taxonomic_data, c("SPECIES_CD", "COMNAME")))
-  
-  p   <-  ggplot(a, aes(YEAR, biomass, group = REGION, color = REGION)) +
-    geom_point() +
-    geom_line() +
-    geom_errorbar(aes(ymin = biomass - SE, ymax = biomass + SE), width = 0.35) +
-    ylab("Biomass") +
-    xlab("Year")
-  
-  return(p)
-  
-}
 
-create_spp_page <- function(df, spp, title) {
+prot_significance_dens <- drto_psu_density %>% 
+  group_by(SPECIES_CD) %>% 
+  nest() %>% 
+  mutate(exp_tests = map(.x=data, ~kruskal.test(density~PROT, data=.) %>% 
+                        tidy())) %>% 
+  unnest(exp_tests) %>% 
+  mutate(p.exp = p.adjust(p.value, method = "BH")) %>% 
+  select(SPECIES_CD, data, p.exp) %>% 
+  filter(p.exp < 0.05)
 
-  density <-    plot_density(df, spp)
-  occurrence <- plot_occurrence(df, spp)
-  biomass <-    plot_biomass(df, spp)
-  
-  page <- grid.arrange(
-    density,
-    occurrence,
-    biomass,
-    ncol = 1,
-    top = textGrob(title,gp=gpar(fontsize=20,font=3))
-  )
-   
-  return(page)
-}
-
+prot_pairwise_dens <- prot_significance_dens %>% 
+  mutate(pairwise  = map(.x= data,
+                         ~pairwise.wilcox.test(x = .x$density,
+                                               g = .x$PROT,
+                                               p.adjust.method = "BH") %>% 
+                           tidy())) %>% 
+  unnest(pairwise) %>% 
+  filter(p.value < 0.05)
